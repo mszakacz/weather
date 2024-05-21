425ba14b161e8f4cf0458ba2fda48c5386bf1f8a
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-21 16:54:11 +0200

    feat(weather_api_client): create WeatherDataWeather

---
+++ packages/weather_api_client/lib/src/models/weather_data_weather.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'weather_data_weather.freezed.dart';
+part 'weather_data_weather.g.dart';
+
+@freezed
+
+/// WeatherData.weather
+class WeatherDataWeather with _$WeatherDataWeather {
+  /// WeatherDataWeather const constructor
+  const factory WeatherDataWeather({
+    required String icon,
+  }) = _WeatherDataWeather;
+
+  /// get WeatherDataWeather instance from json
+  factory WeatherDataWeather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherDataWeatherFromJson(json);
+}
