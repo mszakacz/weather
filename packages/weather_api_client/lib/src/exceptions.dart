a61adb69511314a8628ca924b349e3f7e0c2bdeb
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-21 06:54:45 +0200

    feat(weather_api_client): create GetWeatherForecastFailure, WeatherForecastDeserializationFailure

---
+++ packages/weather_api_client/lib/src/exceptions.dart
@@ -0,0 +1,5 @@
+/// Exception thrown when getWeather request fails.
+class GetWeatherForecastFailure implements Exception {}
+
+/// Exception thrown when WeatherForecast from json fails.
+class WeatherForecastDeserializationFailure implements Exception {}
