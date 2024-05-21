d2c1fb78e600905181951bd8fe47bdc35b75855a
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-21 16:54:37 +0200

    feat(weather_api_client): generate WeatherDataWeather serialization

---
+++ packages/weather_api_client/lib/src/models/weather_data_weather.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_data_weather.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$WeatherDataWeatherImpl _$$WeatherDataWeatherImplFromJson(
+  Map<String, dynamic> json,
+) =>
+    _$WeatherDataWeatherImpl(
+      icon: json['icon'] as String,
+    );
+
+Map<String, dynamic> _$$WeatherDataWeatherImplToJson(
+  _$WeatherDataWeatherImpl instance,
+) =>
+    <String, dynamic>{
+      'icon': instance.icon,
+    };
