c3a15bb92ec7f1ff1da4f15fa7c26005c6a3c46f
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-21 16:55:02 +0200

    feat(weather_api_client): generate WeatherData data class

---
+++ packages/weather_api_client/lib/src/models/weather_data.freezed.dart
@@ -0,0 +1,209 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'weather_data.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+WeatherData _$WeatherDataFromJson(Map<String, dynamic> json) {
+  return _WeatherData.fromJson(json);
+}
+
+/// @nodoc
+mixin _$WeatherData {
+  int get dt => throw _privateConstructorUsedError;
+  WeatherDataMain get main => throw _privateConstructorUsedError;
+  List<WeatherDataWeather> get weather => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $WeatherDataCopyWith<WeatherData> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $WeatherDataCopyWith<$Res> {
+  factory $WeatherDataCopyWith(
+          WeatherData value, $Res Function(WeatherData) then) =
+      _$WeatherDataCopyWithImpl<$Res, WeatherData>;
+  @useResult
+  $Res call({int dt, WeatherDataMain main, List<WeatherDataWeather> weather});
+
+  $WeatherDataMainCopyWith<$Res> get main;
+}
+
+/// @nodoc
+class _$WeatherDataCopyWithImpl<$Res, $Val extends WeatherData>
+    implements $WeatherDataCopyWith<$Res> {
+  _$WeatherDataCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? dt = null,
+    Object? main = null,
+    Object? weather = null,
+  }) {
+    return _then(_value.copyWith(
+      dt: null == dt
+          ? _value.dt
+          : dt // ignore: cast_nullable_to_non_nullable
+              as int,
+      main: null == main
+          ? _value.main
+          : main // ignore: cast_nullable_to_non_nullable
+              as WeatherDataMain,
+      weather: null == weather
+          ? _value.weather
+          : weather // ignore: cast_nullable_to_non_nullable
+              as List<WeatherDataWeather>,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $WeatherDataMainCopyWith<$Res> get main {
+    return $WeatherDataMainCopyWith<$Res>(_value.main, (value) {
+      return _then(_value.copyWith(main: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$WeatherDataImplCopyWith<$Res>
+    implements $WeatherDataCopyWith<$Res> {
+  factory _$$WeatherDataImplCopyWith(
+          _$WeatherDataImpl value, $Res Function(_$WeatherDataImpl) then) =
+      __$$WeatherDataImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({int dt, WeatherDataMain main, List<WeatherDataWeather> weather});
+
+  @override
+  $WeatherDataMainCopyWith<$Res> get main;
+}
+
+/// @nodoc
+class __$$WeatherDataImplCopyWithImpl<$Res>
+    extends _$WeatherDataCopyWithImpl<$Res, _$WeatherDataImpl>
+    implements _$$WeatherDataImplCopyWith<$Res> {
+  __$$WeatherDataImplCopyWithImpl(
+      _$WeatherDataImpl _value, $Res Function(_$WeatherDataImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? dt = null,
+    Object? main = null,
+    Object? weather = null,
+  }) {
+    return _then(_$WeatherDataImpl(
+      dt: null == dt
+          ? _value.dt
+          : dt // ignore: cast_nullable_to_non_nullable
+              as int,
+      main: null == main
+          ? _value.main
+          : main // ignore: cast_nullable_to_non_nullable
+              as WeatherDataMain,
+      weather: null == weather
+          ? _value._weather
+          : weather // ignore: cast_nullable_to_non_nullable
+              as List<WeatherDataWeather>,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$WeatherDataImpl implements _WeatherData {
+  const _$WeatherDataImpl(
+      {required this.dt,
+      required this.main,
+      required final List<WeatherDataWeather> weather})
+      : _weather = weather;
+
+  factory _$WeatherDataImpl.fromJson(Map<String, dynamic> json) =>
+      _$$WeatherDataImplFromJson(json);
+
+  @override
+  final int dt;
+  @override
+  final WeatherDataMain main;
+  final List<WeatherDataWeather> _weather;
+  @override
+  List<WeatherDataWeather> get weather {
+    if (_weather is EqualUnmodifiableListView) return _weather;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_weather);
+  }
+
+  @override
+  String toString() {
+    return 'WeatherData(dt: $dt, main: $main, weather: $weather)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$WeatherDataImpl &&
+            (identical(other.dt, dt) || other.dt == dt) &&
+            (identical(other.main, main) || other.main == main) &&
+            const DeepCollectionEquality().equals(other._weather, _weather));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, dt, main, const DeepCollectionEquality().hash(_weather));
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$WeatherDataImplCopyWith<_$WeatherDataImpl> get copyWith =>
+      __$$WeatherDataImplCopyWithImpl<_$WeatherDataImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$WeatherDataImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _WeatherData implements WeatherData {
+  const factory _WeatherData(
+      {required final int dt,
+      required final WeatherDataMain main,
+      required final List<WeatherDataWeather> weather}) = _$WeatherDataImpl;
+
+  factory _WeatherData.fromJson(Map<String, dynamic> json) =
+      _$WeatherDataImpl.fromJson;
+
+  @override
+  int get dt;
+  @override
+  WeatherDataMain get main;
+  @override
+  List<WeatherDataWeather> get weather;
+  @override
+  @JsonKey(ignore: true)
+  _$$WeatherDataImplCopyWith<_$WeatherDataImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
