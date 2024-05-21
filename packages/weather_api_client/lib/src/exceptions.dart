1d694b4791b7dffda69c80a40b8dbd7d8f955709
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-21 20:15:30 +0200

    feat(weather_api_client): GetLocationsFailure, LocationDataDeserializationFailure

--- packages/weather_api_client/lib/src/exceptions.dart
+++ packages/weather_api_client/lib/src/exceptions.dart
@@ -3,3 +3,9 @@ class GetWeatherForecastFailure implements Exception {}
 
 /// Exception thrown when WeatherForecast from json fails.
 class WeatherForecastDeserializationFailure implements Exception {}
+
+/// Exception thrown when getLocations request fails.
+class GetLocationsFailure implements Exception {}
+
+/// Exception thrown when LocationData from json fails.
+class LocationDataDeserializationFailure implements Exception {}
