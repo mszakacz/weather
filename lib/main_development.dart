1527e782dc713ff307eb606aa4c9443d85bc1376
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-21 06:53:47 +0200

    feat(main): init WeatherApiClient and test call getWeatherForecast

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,16 @@
 import 'package:weather/app/app.dart';
 import 'package:weather/bootstrap.dart';
+import 'package:weather_api_client/weather_api_client.dart';
 
-void main() {
-  bootstrap(() => const App());
+void main() async {
+  await bootstrap(
+    () async {
+      final weatherApiClient = WeatherApiClient();
+      await weatherApiClient.getWeatherForecast(
+        lat: '30.40',
+        lon: '40.10',
+      );
+      return const App();
+    },
+  );
 }
