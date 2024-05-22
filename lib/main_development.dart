632d8c7dd089aecc47535296c75a2d9b9422b890
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-22 20:00:03 +0200

    refactor: development comments

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,21 @@
 import 'package:weather/app/app.dart';
 import 'package:weather/bootstrap.dart';
+// import 'package:weather_api_client/weather_api_client.dart';
+// import 'package:weather_repository/weather_repository.dart';
 
 void main() async {
   await bootstrap(
     () async {
+      // final weatherApiClient = WeatherApiClient();
+      // final weatherRepository = WeatherRepository(
+      //   weatherApiClient: weatherApiClient,
+      // );
+
+      // final forecast = await weatherRepository.getWeatherForecast(
+      //   lat: 53.13248860,
+      //   lon: 23.16884030,
+      // );
+
       return const App();
     },
   );
