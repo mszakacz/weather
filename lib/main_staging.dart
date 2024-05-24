2f849c295a50a7f55bafd8f7132cbe185bc09a5d
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-24 21:19:12 +0200

    feat(main): app takes weatherRepository

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,6 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:location_api_client/location_api_client.dart';
 import 'package:weather/app/app.dart';
 import 'package:weather/bootstrap.dart';
+import 'package:weather_api_client/weather_api_client.dart';
+import 'package:weather_repository/weather_repository.dart';
 
-void main() {
-  bootstrap(() => const App());
+void main() async {
+  await bootstrap(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      final weatherApiClient = WeatherApiClient();
+      final locationApiClient = LocationApiClient();
+      final weatherRepository = WeatherRepository(
+        weatherApiClient: weatherApiClient,
+        locationApiClient: locationApiClient,
+      );
+
+      return App(
+        weatherRepository: weatherRepository,
+      );
+    },
+  );
 }
