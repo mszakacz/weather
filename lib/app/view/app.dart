2cecb0ad693e3dfec8d7fe7023a063c6dc0dd572
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-24 21:18:44 +0200

    feat(app): provide WeatherRepository and navigate to HomePage

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,22 +1,29 @@
+import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:weather/counter/counter.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/app/home/view/home_page.dart';
+
 import 'package:weather/l10n/l10n.dart';
+import 'package:weather_repository/weather_repository.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({
+    required WeatherRepository weatherRepository,
+    super.key,
+  }) : _weatherRepository = weatherRepository;
+
+  final WeatherRepository _weatherRepository;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        useMaterial3: true,
+    return RepositoryProvider.value(
+      value: _weatherRepository,
+      child: MaterialApp(
+        theme: AppTheme.normal,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: const HomePage(),
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
     );
   }
 }
