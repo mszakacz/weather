1c52022ac0ea3e47b84e6af63b4151ca7ffead32
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-24 20:01:48 +0200

    feat(app_ui): create AppSpacing

---
+++ packages/app_ui/lib/src/spacing/app_spacing.dart
@@ -0,0 +1,32 @@
+/// Default Spacing in App UI.
+abstract class AppSpacing {
+  /// The default unit of spacing
+  static const double spaceUnit = 16;
+
+  /// XXXS spacing value (1pt)
+  static const double xxxs = 0.0625 * spaceUnit;
+
+  /// XXS spacing value (2pt)
+  static const double xxs = 0.125 * spaceUnit;
+
+  /// XS spacing value (4pt)
+  static const double xs = 0.25 * spaceUnit;
+
+  /// Small spacing value (8pt)
+  static const double s = 0.5 * spaceUnit;
+
+  /// Medium spacing value (12pt)
+  static const double m = 0.75 * spaceUnit;
+
+  /// Large spacing value (16pt)
+  static const double l = spaceUnit;
+
+  /// Extra large spacing value (24pt)
+  static const double xl = 1.5 * spaceUnit;
+
+  /// XXL spacing value (40pt)
+  static const double xxl = 2.5 * spaceUnit;
+
+  /// XXXL pacing value (64pt)
+  static const double xxxl = 4 * spaceUnit;
+}
