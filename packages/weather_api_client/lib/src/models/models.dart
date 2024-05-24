347efbb712a8f32ef4dd5ba9bc3b5093205eb351
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-24 18:59:36 +0200

    feat(weather_api_client): export WeatherDataWind class

--- packages/weather_api_client/lib/src/models/models.dart
+++ packages/weather_api_client/lib/src/models/models.dart
@@ -3,4 +3,5 @@ export './weather_data.dart';
 export './weather_data_city.dart';
 export './weather_data_main.dart';
 export './weather_data_weather.dart';
+export './weather_data_wind.dart';
 export './weather_forecast_data.dart';
