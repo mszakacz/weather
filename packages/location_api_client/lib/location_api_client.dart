8b3dade6da986d69ed2a6130f81929031a35c6f8
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-24 10:32:28 +0200

    feat(location_api_client): create library

---
+++ packages/location_api_client/lib/location_api_client.dart
@@ -0,0 +1,7 @@
+/// Flutter package that handles location requests
+library;
+
+export 'package:geolocator/geolocator.dart' show Position;
+
+export 'src/exceptions.dart';
+export 'src/location_api_client.dart';
