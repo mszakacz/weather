f249002e55d18b2769320093c22ef368ebbdfbca
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-25 07:50:55 +0200

    refactor: move home to the lib folder

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,7 +1,7 @@
 import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weather/app/home/view/home_page.dart';
+import 'package:weather/home/view/home_page.dart';
 
 import 'package:weather/l10n/l10n.dart';
 import 'package:weather_repository/weather_repository.dart';
