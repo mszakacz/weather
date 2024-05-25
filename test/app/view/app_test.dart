3e5c99f262ad8657ed3dd80bf8d4d3487db99d94
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-25 07:50:58 +0200

    refactor: move home to the lib folder

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:weather/app/app.dart';
-import 'package:weather/app/home/view/home_page.dart';
+import 'package:weather/home/view/home_page.dart';
 import 'package:weather_repository/weather_repository.dart';
 
 class MockWeatherRepository extends Mock implements WeatherRepository {}
