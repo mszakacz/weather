8b88349c6af64e15a44f1b7e5fd0ab4583476dec
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-26 11:15:53 +0200

    test: create helpers

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,15 +1,26 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:weather/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:weather/l10n/l10n.dart';
+import 'package:weather_repository/weather_repository.dart';
 
-// extension PumpApp on WidgetTester {
-//   Future<void> pumpApp(Widget widget) {
-//     return pumpWidget(
-//       MaterialApp(
-//         localizationsDelegates: AppLocalizations.localizationsDelegates,
-//         supportedLocales: AppLocalizations.supportedLocales,
-//         home: widget,
-//       ),
-//     );
-//   }
-// }
+class MockWeatherRepository extends Mock implements WeatherRepository {}
+
+extension PumpApp on WidgetTester {
+  Future<void> pumpApp(
+    Widget widget, {
+    MockWeatherRepository? weatherRepository,
+  }) {
+    return pumpWidget(
+      RepositoryProvider.value(
+        value: weatherRepository ?? MockWeatherRepository(),
+        child: MaterialApp(
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: Scaffold(body: widget),
+        ),
+      ),
+    );
+  }
+}
