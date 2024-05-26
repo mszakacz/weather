ecd44bb3d4ab5feae1f4cb930948e4ac9ebed0c4
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-26 21:59:43 +0200

    test(app_ui): test DateTimeExtension longDate

--- packages/app_ui/lib/src/extensions/date_time_extension.dart
+++ packages/app_ui/lib/src/extensions/date_time_extension.dart
@@ -4,4 +4,8 @@ import 'package:intl/intl.dart';
 extension DateTimeExtension on DateTime {
   /// first  letters of the day name
   String get dayNameShort => DateFormat('EEEE').format(this).substring(0, 3);
+
+  /// long DateTime format,
+  /// e.g: Monday, 22.05
+  String get longDate => DateFormat('EEEE, dd MMMM').format(this);
 }
