9ac42b34995ca3572dbefb62399f411753d41450
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-27 10:21:48 +0200

    test(home): test SelectDay

--- test/home/bloc/home_event_test.dart
+++ test/home/bloc/home_event_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:weather/home/bloc/home_bloc.dart';
+import 'package:weather_repository/weather_repository.dart';
 
 void main() {
   group('HomeEvent', () {
@@ -9,6 +10,35 @@ void main() {
         expect(
           const GetWeatherForecast(),
           equals(const GetWeatherForecast()),
+        );
+      });
+    });
+
+    group('SelectDay', () {
+      final dayWeather = DayWeather(
+        day: DateTime(2024, 5, 22),
+        maxTemperature: 24,
+        minTemperature: 20,
+        iconUrl: 'url',
+        condition: 'few clouds',
+        humidity: 69,
+        pressure: 1015,
+        windGust: 2.3,
+      );
+      test('supports value equality', () {
+        expect(
+          SelectDay(dayWeather: dayWeather),
+          equals(
+            SelectDay(dayWeather: dayWeather),
+          ),
+        );
+      });
+      test('props are correct', () {
+        expect(
+          SelectDay(dayWeather: dayWeather).props,
+          equals(<Object?>[
+            dayWeather,
+          ]),
         );
       });
     });
