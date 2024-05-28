62f4ae6f71280bdd3136e0098cf6c6d2c95834d2
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-28 02:19:52 +0200

    feat(weather_repository): saveUnits, getUnits from LocalStorage

--- packages/weather_repository/lib/src/weather_repository.dart
+++ packages/weather_repository/lib/src/weather_repository.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:local_storage/local_storage.dart';
 import 'package:location_api_client/location_api_client.dart';
 import 'package:weather_api_client/weather_api_client.dart';
 import 'package:weather_repository/weather_repository.dart';
@@ -9,11 +10,16 @@ class WeatherRepository {
   const WeatherRepository({
     required WeatherApiClient weatherApiClient,
     required LocationApiClient locationApiClient,
+    required LocalStorage localStorage,
   })  : _weatherApiClient = weatherApiClient,
-        _locationApiClient = locationApiClient;
+        _locationApiClient = locationApiClient,
+        _localStorage = localStorage;
 
   final WeatherApiClient _weatherApiClient;
   final LocationApiClient _locationApiClient;
+  final LocalStorage _localStorage;
+
+  static const _unitsKey = 'units';
 
   /// Get weather forecast
   @visibleForTesting
@@ -21,9 +27,11 @@ class WeatherRepository {
     required double lat,
     required double lon,
   }) async {
+    final units = getUnits();
     final data = await _weatherApiClient.getWeatherForecast(
       lat: lat,
       lon: lon,
+      units: units.name,
     );
 
     return WeatherForecast.fromData(data);
@@ -39,5 +47,20 @@ class WeatherRepository {
     );
 
     return weatherForecast;
+  }
+
+  /// Save weather units
+  Future<void> saveUnits(Units units) async {
+    await _localStorage.save(units.name, _unitsKey);
+  }
+
+  /// Get Weather units
+  Units getUnits() {
+    final data = _localStorage.get(_unitsKey);
+    if (data == null) {
+      return Units.metric;
+    } else {
+      return Units.values.firstWhere((u) => u.name == data);
+    }
   }
 }
