943198fc10099c1e08255408cb62e40d4976c0a6
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-28 02:13:58 +0200

    feat(home): HomeState has units

--- lib/home/bloc/home_state.dart
+++ lib/home/bloc/home_state.dart
@@ -12,28 +12,33 @@ class HomeState extends Equatable {
     required this.status,
     required this.weatherForecast,
     required this.selectedDay,
+    required this.units,
   });
 
   final HomeStatus status;
   final WeatherForecast weatherForecast;
   final DayWeather selectedDay;
+  final Units units;
 
   @override
   List<Object> get props => [
         status,
         weatherForecast,
         selectedDay,
+        units,
       ];
 
   HomeState copyWith({
     HomeStatus? status,
     WeatherForecast? weatherForecast,
     DayWeather? selectedDay,
+    Units? units,
   }) {
     return HomeState(
       status: status ?? this.status,
       weatherForecast: weatherForecast ?? this.weatherForecast,
       selectedDay: selectedDay ?? this.selectedDay,
+      units: units ?? this.units,
     );
   }
 }
