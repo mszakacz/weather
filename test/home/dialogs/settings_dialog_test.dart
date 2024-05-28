4006157feb19cec7575d843a0f5c3405bf1832ae
Maciej Szakacz <maciej.szakacz@exploreit.io>
2024-05-28 13:20:49 +0200

    test(home): test SettingsDialog showing

---
+++ test/home/dialogs/settings_dialog_test.dart
@@ -0,0 +1,77 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:weather/home/home.dart';
+import 'package:weather_repository/weather_repository.dart';
+
+import '../../helpers/helpers.dart';
+
+void main() {
+  group('SettingsDialog', () {
+    group('show', () {
+      const key = Key('test_button_key');
+
+      testWidgets('renders SettingsDialogContent', (tester) async {
+        await tester.pumpApp(
+          Builder(
+            builder: (context) {
+              return IconButton(
+                key: key,
+                icon: const Icon(Icons.settings_outlined),
+                onPressed: () {
+                  SettingsDialog.show(
+                    context: context,
+                    onChange: (_) {},
+                    selected: Units.metric,
+                  );
+                },
+              );
+            },
+          ),
+        );
+
+        expect(find.byKey(key), findsOneWidget);
+        final testButton = find.byKey(key);
+
+        await tester.pumpAndSettle();
+
+        await tester.ensureVisible(testButton);
+        await tester.tap(testButton);
+        await tester.pumpAndSettle();
+
+        expect(find.byType(SettingsDialogContent), findsOneWidget);
+      });
+
+      testWidgets('shows BottomAnimatedDialog', (tester) async {
+        await tester.pumpApp(
+          Builder(
+            builder: (context) {
+              return IconButton(
+                key: key,
+                icon: const Icon(Icons.settings_outlined),
+                onPressed: () {
+                  SettingsDialog.show(
+                    context: context,
+                    onChange: (_) {},
+                    selected: Units.metric,
+                  );
+                },
+              );
+            },
+          ),
+        );
+
+        expect(find.byKey(key), findsOneWidget);
+        final testButton = find.byKey(key);
+
+        await tester.pumpAndSettle();
+
+        await tester.ensureVisible(testButton);
+        await tester.tap(testButton);
+        await tester.pumpAndSettle();
+
+        expect(find.byType(BottomAnimatedDialogWidget), findsOneWidget);
+      });
+    });
+  });
+}
